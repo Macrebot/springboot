0c6905872ddd7c3c9275433c0183e3ee5bd96faf
Macrebot <luis.maldo@protonmail.com>
2024-09-02 19:35:09 +0200

    POST Cliente

--- src/main/java/macrebot/practices/springboot/controllers/CustomerController.java
+++ src/main/java/macrebot/practices/springboot/controllers/CustomerController.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import macrebot.practices.springboot.models.Customer;
@@ -13,7 +16,7 @@ import macrebot.practices.springboot.models.Customer;
 public class CustomerController {
 
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
-            new Customer(1, "Gerardo López", "gerardo1", "contrasena123"),
+            new Customer(1, "Gerardo López", "gerardol", "contrasena123"),
             new Customer(2, "Alejandra García", "alegarcia", "clave456"),
             new Customer(3, "Laura Sanchéz", "lauras", "secreto789"),
             new Customer(4, "Carlos Martínez", "carlosm", "password234")));
@@ -21,5 +24,21 @@ public class CustomerController {
     @GetMapping("/clientes")
     public List<Customer> getCustomers() {
         return this.customers;
+    }
+
+    @GetMapping("/clientes/{username}")
+    public Customer getCustomer(@PathVariable String username) {
+        for (Customer c : this.customers) {
+            if (c.getUsername().equals(username)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/clientes")
+    public Customer postCustomer(@RequestBody Customer customer) {
+        customers.add(customer);
+        return customer;
     }
 }
